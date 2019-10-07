d90391800abcf2b7f3e03bd06c3e2c7300789194
Shyrops <s.sporysh@online-express.ru>
2019-10-07 17:31:51 +0300

    #task-2-ControllersAndRouts Подобие роутинга

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Diactoros\Response\JsonResponse;
 use Zend\Diactoros\ServerRequestFactory;
 use Zend\HttpHandlerRunner\Emitter\SapiEmitter;
 
@@ -11,14 +12,45 @@ require 'vendor/autoload.php';
 
 $request = ServerRequestFactory::fromGlobals();
 
+//### Preprocessing
+//
+//if (preg_match('#json#i', $request->getHeader('Content-Type'))) {
+//    $request = $request->withParsedBody(json_decode($request->getBody()->getContents()));
+//}
+
 ### Action
 
-$name = $request->getQueryParams()['name'] ?? 'Guest';
+$path = $request->getUri()->getPath();
 
-$response = (new HtmlResponse('Hello, ' . $name . '!'))
-    ->withHeader('X-Developer', 'Sporysh Semyon');
+if ($path == '/') {
+    $name = $request->getQueryParams()['name'] ?? 'Guest';
+    $response = new HtmlResponse('Hello ' . $name . '!');
+} elseif ($path == '/about') {
+    $response = new HtmlResponse('This is simple site');
+} elseif ($path == '/blog') {
+    $response = new JsonResponse([
+        ['id' => 1, 'title' => 'First blog'],
+        ['id' => 2, 'title' => 'Second blog']
+    ], 200);
+} elseif (preg_match('#^/blog/(?P<id>\d+)#i', $path, $matches)) {
+    $id = $matches['id'];
+    if ($id > 2) {
+        $response = new JsonResponse(['error' => 'There is no such page'], 404);
+    } else {
+        $response = new JsonResponse(['id' => $id, 'title' => 'Post #' . $id    ]);
+    }
+
+} else {
+    $response = new JsonResponse(['error' => 'Undefined page', 404]);
+}
+
+
+
+### Postprocessing
+
+$response->withHeader('X-Developer', 'Sporysh Semyon');
 
 ### Sending
 
-$emiter = new SapiEmitter();
-$emiter->emit($response);
+$emitter = new SapiEmitter();
+$emitter->emit($response);
