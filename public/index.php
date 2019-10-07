d93bff8bb4d5dee30e46c78fe5e4f7348799b17e
Shyrops <s.sporysh@online-express.ru>
2019-10-07 18:06:26 +0300

    #task-2-ControllersAndRouts Перешли на использование анонимных функций в экшенах

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Diactoros\Response\JsonResponse;
 use Zend\Diactoros\ServerRequestFactory;
@@ -21,29 +22,41 @@ $request = ServerRequestFactory::fromGlobals();
 ### Action
 
 $path = $request->getUri()->getPath();
+$action = null;
 
 if ($path == '/') {
-    $name = $request->getQueryParams()['name'] ?? 'Guest';
-    $response = new HtmlResponse('Hello ' . $name . '!');
-} elseif ($path == '/about') {
-    $response = new HtmlResponse('This is simple site');
-} elseif ($path == '/blog') {
-    $response = new JsonResponse([
-        ['id' => 1, 'title' => 'First blog'],
-        ['id' => 2, 'title' => 'Second blog']
-    ], 200);
-} elseif (preg_match('#^/blog/(?P<id>\d+)#i', $path, $matches)) {
-    $id = $matches['id'];
-    if ($id > 2) {
-        $response = new JsonResponse(['error' => 'There is no such page'], 404);
-    } else {
-        $response = new JsonResponse(['id' => $id, 'title' => 'Post #' . $id    ]);
-    }
+    $action = function (ServerRequestInterface $request) {
+        $name = $request->getQueryParams()['name'] ?? 'Guest';
 
+        return new HtmlResponse('Hello ' . $name . '!');
+    };
+} else if ($path == '/about') {
+    $action = function (ServerRequestInterface $request) {
+        return new HtmlResponse('This is simple site');
+    };
+} else if ($path == '/blog') {
+    $action = function (ServerRequestInterface $request) {
+        return new JsonResponse([
+            ['id' => 1, 'title' => 'First blog'],
+            ['id' => 2, 'title' => 'Second blog'],
+        ], 200);
+    };
+
+} else if (preg_match('#^/blog/(?P<id>\d+)#i', $path, $matches)) {
+    $request = $request->withAttribute('id', $matches['id']);
+    $action = function (ServerRequestInterface $request) {
+        $id = $request->getAttribute('id');
+        if ($id > 2) {
+            return new JsonResponse(['error' => 'There is no such page'], 404);
+        }
+        return new JsonResponse(['id' => $id, 'title' => 'Post #' . $id]);
+    };
+}
+if ($action) {
+    $response = $action($request);
 } else {
     $response = new JsonResponse(['error' => 'Undefined page', 404]);
 }
-
 
 
 ### Postprocessing
